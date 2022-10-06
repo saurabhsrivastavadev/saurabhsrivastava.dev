2498de44db9925067bcd7b7d318a2a180eac222e
Saurabh Srivastava <me@saurabhsrivastava.dev>
2022-10-06 23:07:11 +0530

    Completed header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'linkedin': '#0a66c2',
+      'github': '#171515',
+      'google': '#bb001b'
+    }
   },
   plugins: [],
 }
