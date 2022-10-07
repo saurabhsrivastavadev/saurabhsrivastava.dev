c36869ac625f662998a5f24127da67a3a3306efb
Saurabh Srivastava <me@saurabhsrivastava.dev>
2022-10-07 18:07:28 +0530

    Some design updates, updated skills and experience sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,13 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
-    colors: {
-      'linkedin': '#0a66c2',
-      'github': '#171515',
-      'google': '#bb001b'
-    }
+    extend: {
+      colors: {
+        'linkedin': '#0a66c2',
+        'github': '#171515',
+        'google': '#bb001b'
+      }  
+    },
   },
   plugins: [],
 }
